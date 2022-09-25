f8f9feb414a7250442c13a7a2d5cd84fd665a897
adrianlopezv <adrian.1505@hotmail.com>
2022-09-25 13:12:23 +0200

    first commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    //Not learning Java but reviewing it and practicing it with my new course
+    //Public means that it can be called in any part of the app
+    public static void main(String [] args){
+        System.out.println("Hello world from Java");
+    }
+
+}
